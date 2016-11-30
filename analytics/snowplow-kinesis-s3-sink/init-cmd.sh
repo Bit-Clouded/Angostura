0f139fbef2e245ffc80d1a6c224a303baa418110
Jason Chen <jason.chen@bit-clouded.com>
2016-11-30 19:12:13 +1100

    S3 sink from snowplow.

---
+++ analytics/snowplow-kinesis-s3-sink/init-cmd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+PVT_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+
+COLLECTOR_PORT=80
+#cookies
+COOKIE_EXPIRATION="36500 days" #100 years
+COOKIE_NAME="bitclouded-snowplow"
+#backoffs
+MIN_BACKOFF=1000
+MAX_BACKOFF=100000
+# Need to observe kinesis shard limit
+BYTE_THRESHOLD=50000000 #50MB
+RECORD_THRESHOLD=5000000 #5mil
+TIME_THRESHOLD=
+
+LOG_LEVEL=WARN
+
+# Externally Fed Variables
+sed -i "s/{{sinkKinesisInStreamName}}/$STREAM_SOURCE/g" ./config.hocon
+sed -i "s/{{sinkKinesisAppName}}/$DDB_CHECKPOINT/g" ./config.hocon
+sed -i "s/{{sinkKinesisOutStreamName}}/$STREAM_BAD/g" ./config.hocon
+sed -i "s/{{sinkKinesisS3Bucket}}/$BUCKET_NAME/g" ./config.hocon
+
+# Internal Variables
+sed -i "s/{{sinkKinesisRegion}}/$EC2_REGION/g" ./config.hocon
+sed -i "s/{{sinkKinesisS3Region}}/$EC2_REGION/g" ./config.hocon
+
+sed -i "s/{{sinkLzoBufferByteThreshold}}/$BYTE_THRESHOLD/g" ./config.hocon
+sed -i "s/{{sinkLzoBufferRecordThreshold}}/$RECORD_THRESHOLD/g" ./config.hocon
+sed -i "s/{{sinkLzoBufferTimeThreshold}}/$TIME_THRESHOLD/g" ./config.hocon
+
+# Change log level
+sed -i "s/{{sinkLzoLogLevel}}/$LOG_LEVEL/g" ./config.hocon
+
+echo Starting...
+java -jar ./$SSE_JAR --config ./config.hocon --resolver file:./resolver.config
