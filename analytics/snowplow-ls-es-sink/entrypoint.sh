c42a4d6ee74fea805475565a6159b7aff235c35b
Jason Chen <jason.chen@bit-clouded.com>
2016-11-30 20:37:59 +1100

    bug fix.

--- analytics/snowplow-ls-es-sink/entrypoint.sh
+++ analytics/snowplow-ls-es-sink/entrypoint.sh
@@ -4,9 +4,9 @@ EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availa
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 PVT_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
 
-sed -i "s/{{aws-region}}/$EC2_REGION/g" ./ls-aws-cwl.conf
-sed -i "s/{{stream-name}}/$STREAM_SOURCE/g" ./ls-aws-cwl.conf
-sed -i "s/{{checkpoint-ddb}}/$DDB_CHECKPOINT/g" ./ls-aws-cwl.conf
-sed -i "s/{{es-host}}/$ES_HOST/g" ./ls-aws-cwl.conf
+sed -i "s/{{aws-region}}/$EC2_REGION/g" ./ls-snowplow-es.conf
+sed -i "s/{{stream-name}}/$STREAM_SOURCE/g" ./ls-snowplow-es.conf
+sed -i "s/{{checkpoint-ddb}}/$DDB_CHECKPOINT/g" ./ls-snowplow-es.conf
+sed -i "s/{{es-host}}/$ES_HOST/g" ./ls-snowplow-es.conf
 
 logstash -f ./ls-snowplow-es.conf
