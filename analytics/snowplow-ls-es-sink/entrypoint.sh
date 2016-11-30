847d14b3011a765da5d8f65d5b6c9c5783cd4e33
Jason Chen <jason.chen@bit-clouded.com>
2016-11-30 20:33:16 +1100

    Logstash for Snowplow

---
+++ analytics/snowplow-ls-es-sink/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+PVT_IP=`curl -s http://169.254.169.254/latest/meta-data/local-ipv4`
+
+sed -i "s/{{aws-region}}/$EC2_REGION/g" ./ls-aws-cwl.conf
+sed -i "s/{{stream-name}}/$STREAM_SOURCE/g" ./ls-aws-cwl.conf
+sed -i "s/{{checkpoint-ddb}}/$DDB_CHECKPOINT/g" ./ls-aws-cwl.conf
+sed -i "s/{{es-host}}/$ES_HOST/g" ./ls-aws-cwl.conf
+
+logstash -f ./ls-snowplow-es.conf
