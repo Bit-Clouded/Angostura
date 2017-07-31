8e1f07c1cdb21aeba153909ce65013313fda6355
jason.chen <jason.chen@bit-clouded.com>
2017-07-31 23:15:38 +1000

    Add packer for ubuntu base AWS AMI

---
+++ packer/ubuntu-base/install-ssm-agent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /tmp
+
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
