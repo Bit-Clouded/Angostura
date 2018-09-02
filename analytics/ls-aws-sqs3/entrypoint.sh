f0a613533df28549cb9908fb3d9d8e2a020735f0
imomou <ko.sam.wang@gmail.com>
2018-09-01 23:56:34 -0700

    logstash ingestion for AWS billing (#3)
    Adds log ingestion for aws billing logs.

--- analytics/ls-aws-sqs3/entrypoint.sh
+++ analytics/ls-aws-sqs3/entrypoint.sh
@@ -9,6 +9,7 @@ echo Elasticsearch Host: $ES_HOST
 echo Raw Log Bucket: $RAW_BUCKET
 echo S3 Access Log Bucket: $S3_BUCKET
 echo CF Access Log Bucket: $CF_BUCKET
+echo Bill Bucket: $BILL_BUCKET
 echo =====================================
 echo Region: $EC2_REGION
 
@@ -18,5 +19,6 @@ sed -i "s/{{es-host}}/$ES_HOST/g" ./ls-aws-sqs3.conf
 sed -i "s/{{raw-log-bucket}}/$RAW_BUCKET/g" ./ls-aws-sqs3.conf
 sed -i "s/{{s3-access-log-bucket}}/$S3_BUCKET/g" ./ls-aws-sqs3.conf
 sed -i "s/{{cf-access-log-bucket}}/$CF_BUCKET/g" ./ls-aws-sqs3.conf
+sed -i "s/{{bill-bucket}}/$BILL_BUCKET/g" ./ls-aws-sqs3.conf
 
 logstash -f ./ls-aws-sqs3.conf
