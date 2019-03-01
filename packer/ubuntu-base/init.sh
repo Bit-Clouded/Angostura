2aabfc02e01620b53729aa4a8bb164e8932dbfbc
Teakay <teakay.starwyn@gmail.com>
2019-03-01 11:15:49 +1100

    new docker install method

--- packer/ubuntu-base/init.sh
+++ packer/ubuntu-base/init.sh
@@ -12,6 +12,23 @@ cd /opt/aws/amazon-cloudwatch-agent/bin/
 sudo wget https://prod-cicdshared-utility-1hzwhron259mj.s3-eu-west-1.amazonaws.com/CloudWatchAgentConfig/linux-agent-config.json
 sudo wget https://prod-cicdshared-utility-1hzwhron259mj.s3-eu-west-1.amazonaws.com/CloudWatchAgentConfig/configureCloudWatch.sh
 
-#copied from previous setup script install-docker.sh 
-curl -fsSL get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+
+
+#install docker
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
