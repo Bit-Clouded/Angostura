182c3f8a4eb26b6189b17128367a543fb735081f
Teakay <teakay.starwyn@gmail.com>
2019-03-01 11:24:55 +1100

    adjusted docker install method

--- packer/ubuntu-base/init.sh
+++ packer/ubuntu-base/init.sh
@@ -24,6 +24,8 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
@@ -31,4 +33,4 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce containerd.io
