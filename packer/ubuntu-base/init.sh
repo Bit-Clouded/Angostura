4eec9c2ef4d8491536ad45890d11878db4661ffb
Teakay <teakay.starwyn@gmail.com>
2019-03-01 12:00:42 +1100

    config cloud watch agent during docker build for linux

--- packer/ubuntu-base/init.sh
+++ packer/ubuntu-base/init.sh
@@ -12,7 +12,8 @@ cd /opt/aws/amazon-cloudwatch-agent/bin/
 sudo wget https://prod-cicdshared-utility-1hzwhron259mj.s3-eu-west-1.amazonaws.com/CloudWatchAgentConfig/linux-agent-config.json
 sudo wget https://prod-cicdshared-utility-1hzwhron259mj.s3-eu-west-1.amazonaws.com/CloudWatchAgentConfig/configureCloudWatch.sh
 
-
+#configure cloudwatch-agent
+sudo amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:linux-agent-config.json -s
 
 #install docker
 sudo apt-get update
