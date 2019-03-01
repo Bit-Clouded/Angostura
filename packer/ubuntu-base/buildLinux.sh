92c6ccaf6f27f97bd7d7b0362e0bfabc58479431
Teakay <teakay.starwyn@gmail.com>
2019-03-01 15:54:48 +1100

    trying to copy config file to image using file provisioner

--- packer/ubuntu-base/buildLinux.sh
+++ packer/ubuntu-base/buildLinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-aws s3 cp config/linux-agent-config.json s3://prod-cicdshared-utility-1hzwhron259mj/CloudWatchAgentConfig/linux-agent-config.json --acl public-read
-aws s3 cp config/configureCloudWatch.sh s3://prod-cicdshared-utility-1hzwhron259mj/CloudWatchAgentConfig/configureCloudWatch.sh --acl public-read
+#aws s3 cp config/linux-agent-config.json s3://prod-cicdshared-utility-1hzwhron259mj/CloudWatchAgentConfig/linux-agent-config.json --acl public-read
+#aws s3 cp config/configureCloudWatch.sh s3://prod-cicdshared-utility-1hzwhron259mj/CloudWatchAgentConfig/configureCloudWatch.sh --acl public-read
 
 
 ../packer build linuxAmi.json
